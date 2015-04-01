230329058d0330b8f59d14f27b9bba755420f5ad
ThyRex <ThyRex@users.noreply.github.com>
2015-03-31 21:40:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<html>
-<head>
 <?php
 ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
 session_start();
 ?>
+<html>
+<head>
 </head>
 <body> 
 
@@ -13,36 +13,42 @@ session_start();
 	<input type="submit" value="Submit">
 </form>
 
+
+<form action="<?php print($_SERVER['SCRIPT_NAME'])?>" method="get">
+	<input type="submit" value="Save" name="save">
+	<input type="submit" value="Clear" name="reset">
+</form>
+
 <?php
 if(!isset($_SESSION['entries'])){
 	$_SESSION['entries'] = array();
 }
 if(isset($_GET["entry"])){
 	$temp = htmlentities($_GET["entry"]);
-}
-if(isset($temp) && !empty($temp)){
 	$entries = &$_SESSION['entries'];
 	$entries []= $temp;
 }
-?>
 
-<form action="<?php print($_SERVER['SCRIPT_NAME'])?>" method="get">
-	<input type="button" value="Save" onclick=<?php 
+if(isset($_GET["save"])){
 	$fh = fopen("entries.json", 'w');
 	if($fh === false)
 		die("Failed to open entries.json for writing.");
 	else
 	{
-		fwrite($fh, json_encode($entries));
+		fwrite($fh, json_encode($_SESSION['entries']));
 		fclose($fh);
-	} ?>
->
-</form>
+	}
+}
 
-<?php
+ob_start();
 foreach(array_reverse($_SESSION['entries']) as $entry){
 	echo $entry;
 	echo "<br>";
+}
+
+if(isset($_GET["reset"])){
+	unset($_SESSION['entries']);
+	ob_end_clean();
 }
 ?>
 
